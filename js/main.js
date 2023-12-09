84c48da297ebd164de0f2ad8b04a42e8004943d3
itsbamee <jan9239@naver.com>
2023-12-09 09:55:09 +0900

    progress를 통한 정해진 반복횟수 안쪽에서의 변화량 적용

--- js/main.js
+++ js/main.js
@@ -7,12 +7,11 @@
   결국 정해진 반복횟수 안에서 원하는 시간동안 모션처리를 하기 위해서는 반복횟수랑 최종 수치값이 변경되야 되는 변화폭을 제어
 */
 
-//"1초동안" 박스가 오른쪽으로 500px위치까지 이동
 const btn = document.querySelector('button');
 const box = document.querySelector('#box');
 const speed = 1000;
 const targetValue = 500;
-let num = 0;
+let count = 0;
 let startTime = 0;
 
 btn.addEventListener('click', () => {
@@ -22,16 +21,22 @@ btn.addEventListener('click', () => {
 });
 
 function move(time) {
-	//timelast : 브라우저가 로딩된 시점부터가 아닌 사용자가 버튼을 클릭한 시점부터의 누적시간
 	let timelast = time - startTime;
-	console.log(timelast);
-	num++;
-	box.style.marginLeft = num + 'px';
 
-	if (num >= 100) {
-		return;
-	}
-	//버튼 클릭시 requestAnimationFrame에 의해서 move함수가 호출되면
-	//move함수 내부적으로 다시 requestAnimationFrame으로 자기 자신을 또 호출하면서 반복실행됨
-	requestAnimationFrame(move);
+	//매 반복횟수마다 현재 걸리는 누적시간을 전체시간으로 나누면 0~1사이의 실수값이 반환 (해당실수 * 100)
+	//progress : 설정한 시간대비 현재 반복되는 수치값의 진행상황을 0~1사이의 실수값으로 반환한 값
+	let progress = timelast / speed;
+	console.log('누적시간', timelast);
+	console.log('진행률', progress);
+	console.log('반복횟수', count++);
+	console.log('-------------------');
+
+	//만약 진행률이 음수가 되거나 1을 넘어서면 각각 0, 1로 강제 보정처리
+	progress < 0 && (progress = 0);
+	progress > 1 && (progress = 1);
+
+	//진행률이 1에 도달하면 원하는 시간이 끝난 시점으로 반복 종료
+	progress < 1 && requestAnimationFrame(move);
+
+	box.style.marginLeft = targetValue * progress + 'px';
 }
