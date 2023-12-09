71d3c4694900ac3c8015b8989e3ee8f622188088
itsbamee <jan9239@naver.com>
2023-12-09 10:14:21 +0900

    anime함수로 호출하기 편하도록 패키징

--- js/main.js
+++ js/main.js
@@ -15,28 +15,36 @@ let count = 0;
 let startTime = 0;
 
 btn.addEventListener('click', () => {
-	startTime = performance.now();
-	console.log('시작시간', startTime);
-	requestAnimationFrame(move);
+	anime(box, {
+		prop: 'margin-top',
+		value: 300,
+		duration: 1500,
+	});
 });
 
+function anime(selector, option) {
+	const startTime = performance.now();
+	requestAnimationFrame(move);
+
+	function move(time) {
+		let timelast = time - startTime;
+		let progress = timelast / option.duration;
+
+		progress < 0 && (progress = 0);
+		progress > 1 && (progress = 1);
+		progress < 1 && requestAnimationFrame(move);
+
+		selector.style[option.prop] = option.value * progress + 'px';
+	}
+}
+
+/*
 function move(time) {
 	let timelast = time - startTime;
-
-	//매 반복횟수마다 현재 걸리는 누적시간을 전체시간으로 나누면 0~1사이의 실수값이 반환 (해당실수 * 100)
-	//progress : 설정한 시간대비 현재 반복되는 수치값의 진행상황을 0~1사이의 실수값으로 반환한 값
 	let progress = timelast / speed;
-	console.log('누적시간', timelast);
-	console.log('진행률', progress);
-	console.log('반복횟수', count++);
-	console.log('-------------------');
 
-	//만약 진행률이 음수가 되거나 1을 넘어서면 각각 0, 1로 강제 보정처리 (console상에는 음수로 뜨게되지만 그 뜬 값을 보정하기 위해 아래 함수를 쓰는 것. 따라서 보정된 것을 console로 확인은 안됨)
 	progress < 0 && (progress = 0);
 	progress > 1 && (progress = 1);
-
-	//진행률이 1에 도달하면 원하는 시간이 끝난 시점으로 반복 종료
 	progress < 1 && requestAnimationFrame(move);
-
-	box.style.marginLeft = targetValue * progress + 'px';
 }
+*/
