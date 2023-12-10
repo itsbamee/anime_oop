f68a0cd669047e0423d8993df0d8566ded350ab4
itsbamee <jan9239@naver.com>
2023-12-10 10:51:52 +0900

    cdn오류 해결

--- js/anime.js
+++ js/anime.js
@@ -1,15 +1,9 @@
 class Anime {
-	//defualt값 숨김처리
 	#defOpt = { duration: 500, easing: [0, 0, 0, 0], callback: null };
 
-	constructor(selector, props, opt) {
-		this.resultOpt = { ...this.#defOpt, ...opt };
-		this.selector = selector;
-		this.startTime = performance.now();
-		this.keys = Object.keys(props);
-		this.values = Object.values(props);
+	constructor() {
+		//초기 인스턴스 복사시 setCDN만 먼저 호출해서 cdn코드 먼저 삽입되도록 처리
 		this.setCDN();
-		this.keys.forEach((key, idx) => this.setValue(key, this.values[idx]));
 	}
 
 	setCDN() {
@@ -17,6 +11,16 @@ class Anime {
 		script.src = 'https://cdn.jsdelivr.net/npm/bezier-easing@2.1.0/dist/bezier-easing.min.js';
 		script.async = true;
 		document.body.appendChild(script);
+	}
+
+	//추후 인스턴스로부터 animate메서드를 직접 호출해야 모션실행되도록 함수 분리
+	animate(selector, props, opt) {
+		this.resultOpt = { ...this.#defOpt, ...opt };
+		this.selector = selector;
+		this.startTime = performance.now();
+		this.keys = Object.keys(props);
+		this.values = Object.values(props);
+		this.keys.forEach((key, idx) => this.setValue(key, this.values[idx]));
 	}
 
 	setValue(key, value) {
@@ -48,8 +52,8 @@ class Anime {
 		let timelast = time - this.startTime;
 		let progress = timelast / this.resultOpt.duration;
 
-		//const easingFunc = BezierEasing(...this.resultOpt.easing);
-		//const easingProgress = easingFunc(progress);
+		const easingFunc = BezierEasing(...this.resultOpt.easing);
+		const easingProgress = easingFunc(progress);
 
 		progress < 0 && (progress = 0);
 		progress > 1 && (progress = 1);
@@ -57,8 +61,8 @@ class Anime {
 			? requestAnimationFrame((time) => this.move(time, key, value, currentValue, isString))
 			: this.resultOpt.callback && this.resultOpt.callback();
 
-		//let result = currentValue + (value - currentValue) * easingProgress;
-		let result = currentValue + (value - currentValue) * progress;
+		let result = currentValue + (value - currentValue) * easingProgress;
+		//let result = currentValue + (value - currentValue) * progress;
 
 		if (isString === 'string') this.selector.style[key] = result + '%';
 		else if (key === 'opacity') this.selector.style[key] = result;
